01ede722ccb07711c58433a9300fc28286fa7911
Shreejana Tandukar <shreejanatandukar2@gmail.com>
2022-11-07 16:29:53 +0545

    first commit

---
+++ client/src/pages/ChangePassword.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+import axios from 'axios'
+
+function ChangePassword() {
+
+    const [currentPassword, setCurrentPassword] = useState("");
+    const [newPassword, setNewPassword] = useState("");
+
+    const changePassword = () => {
+        axios.put("http://localhost:3001/auth/changepassword", 
+            {
+            currentPassword: currentPassword,
+            newPassword: newPassword,
+            }, 
+            {
+                headers: { accessToken: localStorage.getItem("accessToken") },
+            },
+        ).then((response) => {
+            if(response.data.error){
+                alert(response.data.error);
+            }
+        })
+    }
+    return (
+        <div>
+            <h1>Change Your Password</h1>
+            <input type="text" placeholder='Current Password...' onChange={(event) => {
+                setCurrentPassword(event.target.value);
+            }}
+            />
+            <input type="text" placeholder='New Password...' onChange={(event) => {
+                setNewPassword(event.target.value);
+            }} />
+            <button onClick={changePassword}>Save Changes</button>
+        </div>
+    )
+}
+
+export default ChangePassword
